8104f9945cad2b9e4fbde1747b345a3706758266
dstanton <djstanton@gmail.com>
2021-03-29 09:33:51 -0700

    Solution is correct

--- src/com/djstanton/ComplexNumber.java
+++ src/com/djstanton/ComplexNumber.java
@@ -1,4 +1,38 @@
 package com.djstanton;
 
 public class ComplexNumber {
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public void add(double real, double imaginary) {
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+
+    public void add(ComplexNumber number) {
+        this.add(number.real, number.imaginary);
+    }
+
+    public void subtract(double real, double imaginary) {
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+    public void subtract(ComplexNumber number) {
+        this.subtract(number.real, number.imaginary);
+    }
+
 }
